9bbf1d9167351c3623f6238cf29da3b55ca88d63
himanshug3385 <himanshugupta3385@gmail.com>
2024-08-26 15:21:32 +0530

    initial-commit

---
+++ VaccinationMSystem/src/main/java/com/vaccine/VaccinationMSystem/VaccinationMSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vaccine.VaccinationMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationMSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationMSystemApplication.class, args);
+	}
+
+}
---
+++ VaccinationMSystem/src/test/java/com/vaccine/VaccinationMSystem/VaccinationMSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccine.VaccinationMSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationMSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
